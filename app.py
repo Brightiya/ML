1a7b0bf94829c0fade6c4461df91f8e42078e694
Brightiya <bightiyahen@gmail.com>
2024-12-07 03:54:59 +0100

    Initial commit: Add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+
+st.title("Hello Streamlit")
+st.write("This is your first Streamlit app.🎉")
+
+# load data
+iris = load_iris()
+clf = RandomForestClassifier()
+clf.fit(iris.data, iris.target)
+
+st.title("Iris Flower Prediction")
+sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.5)
+sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.5)
+petal_length = st.slider("Petal Length", 1.0, 7.0, 5.5)
+petal_width = st.slider("Petal Width", 2.0, 7.0, 5.5)
+
+prediction = clf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+st.write(f"Predicted class: {iris.target_names[prediction[0]]}")
